8d29a1250e613a0587899920e21ce8b740d990b2
meaar <alberto.ribeiro@aluno.ufca.edu.br>
2023-05-01 12:02:02 -0300

    Downgrade para versão do bootstrap 4.6 e refatoração

--- javascript/script.js
+++ javascript/script.js
@@ -1,44 +1,54 @@
-$(document).ready(function() {
-    $('.select2').select2({
-        ajax: {
-            url: "http://localhost:8080/api/buscar/aeroportos",
-            dataType: 'json',
-            delay: 250,
-            data: function (params) {
-                var query = {
-                    city: params.term
-                }
-                return query;
-            },
-            processResults: function (data, page) {
-                return {
-                    results: data
-                };
-            }
-        },
-        minimumInputLength: 1,
-        templateResult: formatOption,
-        templateSelection: formatSelection
-    });
-});
+// $(document).ready(function() {
+//     $('.selectpicker').selectpicker().ajaxSelectPicker({
+//         ajax: {
+//             url: "http://localhost:8080/api/buscar/aeroportos",
+//             dataType: 'json',
+//             delay: 250,
+//             data: function (params) {
+//                 var query = {
+//                     city: params.term
+//                 }
+//                 return query;
+//             },
+//             preprocessData: function (data) {
+//                 var i, l = data.length, array = [];
+//                 if (l) {
+//                     for (i = 0; i < l; i++) {
+//                         array.push($.extend(true, data[i], {
+//                             text : data[i].Name,
+//                             value: data[i].Email,
+//                             data : {
+//                                 subtext: data[i].Email
+//                             }
+//                         }));
+//                     }
+//                 }
+//                 return array;
+//             }
+//         // minimumInputLength: 1,
+//         // templateResult: formatOption,
+//         // templateSelection: formatSelection
+//         }
+//     });
+// });
 
-function formatOption(aeroporto) {
-    if (aeroporto.loading) {
-      return aeroporto.text;
-    }
-    var $container = $(
-        "<option value="+aeroporto+">"+aeroporto.city+" - "+aeroporto.iata+"</option>"
-    );
-    return $container;
-}
+// function formatOption(aeroporto) {
+//     if (aeroporto.loading) {
+//       return aeroporto.text;
+//     }
+//     var $container = $(
+//         "<option value="+aeroporto+">"+aeroporto.city+" - "+aeroporto.iata+"</option>"
+//     );
+//     return $container;
+// }
   
-function formatSelection(aeroporto) {
-    return aeroporto.city+" - "+aeroporto.iata;
-}
+// function formatSelection(aeroporto) {
+//     return aeroporto.city+" - "+aeroporto.iata;
+// }
 
-async function logJSONData() {
-    const url = "http://localhost:8080/api/buscar/aeroportos"
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    console.log(jsonData);
-}
+// async function logJSONData() {
+//     const url = "http://localhost:8080/api/buscar/aeroportos"
+//     const response = await fetch(url);
+//     const jsonData = await response.json();
+//     console.log(jsonData);
+// }
